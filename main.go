7d33a7ccecbfeb062b5efbf3eb8d06a394bc1f1a
Jacob Gadikian <jacobgadikian@gmail.com>
2019-12-06 11:23:49 -0500

    Error handling and account info

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/walkbean/vsys-sdk-go/vsys"
 	"io/ioutil"
+	"log"
 )
 
 func check(e error) {
@@ -28,10 +29,19 @@ func main() {
 	//initalize account
 	acc := vsys.InitAccount(vsys.Mainnet)
 	acc.BuildFromPrivateKey(pkstring)
+	info, err := acc.GetInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(info.Address)
 	//b := []byte("Lets test right on mainnet")
 	tx := acc.BuildPayment("ARFWV2aphzfZ5VKLk6xgxPEZhumnSgQBU7y", 1e8, []byte{})
 	resp, err := vsys.SendPaymentTx(tx)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(resp.Error)
 	fmt.Println(resp.Id)
-	fmt.Println(err)
 }
