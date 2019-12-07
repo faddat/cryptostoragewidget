b492ffb27ecca7470a6375391749dd66a0974d1e
Jacob Gadikian <jacobgadikian@gmail.com>
2019-12-07 10:28:38 -0500

    modified to use private key

--- main.go
+++ main.go
@@ -18,28 +18,36 @@ func main() {
 	//Connect to VSYS public API
 	vsys.InitApi("https://wallet.v.systems/api", vsys.Mainnet)
 
+	//COMMENTED OUT SEED STUFF
 	//read private key from file
-	seed, err := ioutil.ReadFile("seed")
+	//seed, err := ioutil.ReadFile("seed")
+	//check(err)
+	//seedstring := string(seed)
+
+	privatekey, err := ioutil.ReadFile("privatekey")
 	check(err)
-	seedstring := string(seed)
-	//prints the seed for user verification
-	fmt.Println(seedstring)
+	pkstring := string(privatekey)
+
+
+	//prints the seed or private key for user verification
+	fmt.Println("Your Private Key is: ", pkstring)
 
 
 	//initalize account
 	acc := vsys.InitAccount(vsys.Mainnet)
-	acc.BuildFromSeed(seedstring, 0)
+
+	//acc.BuildFromSeed(seedstring, 0)
+	acc.BuildFromPrivateKey(pkstring)
 	info, err := acc.GetInfo()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//Print generated address
-	fmt.Println("Your address is:")
-	fmt.Println(info.Address)
+	fmt.Println("Your address is: ", info.Address)
 
 	//b := []byte("Lets test right on mainnet")
-	tx := acc.BuildPayment("ARFWV2aphzfZ5VKLk6xgxPEZhumnSgQBU7y", 1e8, []byte{})
+	tx := acc.BuildPayment("ARFWV2aphzfZ5VKLk6xgxPEZhumnSgQBU7y", 1e6, []byte{})
 	resp, err := vsys.SendPaymentTx(tx)
 
 
